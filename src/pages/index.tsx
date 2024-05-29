a5316927e57ed98b81e3fe02208ab9a198a8600c
cf <carter.feld.man@gmail.com>
2024-05-29 14:18:49 +0800

    fixed typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
-              to="/">
+              to="/docs/tutorial/getting_started">
               Run City Rollup Locally
             </Link>
           </div>
